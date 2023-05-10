1fee6b564bbbd54b7649ec4ecf107da3e69b0154
mehtap <mhtpckrgl@gmail.com>
2023-05-10 23:34:24 +0300

    Mehtap10.05.23

--- src/main/java/employee_service_project/EmployeeService.java
+++ src/main/java/employee_service_project/EmployeeService.java
@@ -1,18 +1,20 @@
 package employee_service_project;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class EmployeeService {
-    HashSet<Employee> empset=new HashSet<Employee>();
+    List<Employee> employees =new ArrayList<Employee>();
 
     Employee emp1=new Employee(101, "Shital", 24, "Developer", "IT", 25000);
     Employee emp2=new Employee(102, "Meena", 26, "Tester","CO", 57000);
     Employee emp3=new Employee(103, "Bob", 20, "DevOps Eng","Admin", 5000);
     Employee emp4=new Employee(104, "Max", 27, "System Eng","CO",  70000);
 
-    Scanner sc=new Scanner(System.in);
-    boolean found=false;
+    Scanner scan =new Scanner(System.in);
+    boolean flag =false;
     int id;
     String name;
     int age;
@@ -20,103 +22,105 @@ public class EmployeeService {
     String desiganation;
     double sal;
 
+    //Calisan ekleme (add employee)
+    public void addEmp() {
+        System.out.println("Enter id:");
+        id= scan.nextInt();
+        System.out.println("Enter name");
+        name= scan.next();
+        System.out.println("Enter age");
+        age= scan.nextInt();
+        System.out.println("enter desiganation");
+        desiganation= scan.next();
+        System.out.println("Enter department");
+        department= scan.next();
+        System.out.println("Enter salary");
+        scan.nextDouble();
+
+        Employee emp=new Employee(id, name, age, desiganation, department, sal);
+
+        employees.add(emp);
+        System.out.println(emp);
+        System.out.println("Employtee addeed successsfully");
+
+    }
+
+
     public EmployeeService() {
 
-        empset.add(emp1);
-        empset.add(emp2);
-        empset.add(emp3);
-        empset.add(emp4);
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        employees.add(emp4);
 
     }
 
-    //view all employees
+    //Butun calisanlari listele(wiev all employees
     public void viewAllEmps() {
-        for(Employee emp:empset) {
-            System.out.println(emp);
+        for(Employee w: employees) {
+            System.out.println(w);
         }
     }
-    //view emp based on there id
+    //id numarasina gore calisan listeleme(view emp based on there id
     public void viewEmp(){
 
         System.out.println("Enter id: ");
-        id=sc.nextInt();
-        for(Employee emp:empset) {
-            if(emp.getId()==id) {
-                System.out.println(emp);
-                found=true;
+        id= scan.nextInt();
+        for(Employee w: employees) {
+            if(w.getId()==id) {
+                System.out.println(w);
+                flag =true;
             }
 
         }
-        if(!found) {
+        if(!flag) {
             System.out.println("Employee with this id is not present");
         }
     }
-    //update the employee
+    //calisan guncelleme (update the employee
     public void updateEmp() {
         System.out.println("Enter id: ");
-        id=sc.nextInt();
-        boolean found=false;
-        for(Employee emp:empset) {
-            if(emp.getId()==id) {
+        id= scan.nextInt();
+        boolean flag=false;
+        for(Employee w: employees) {
+            if(w.getId()==id) {
                 System.out.println("Enter name: ");
-                name=sc.next();
+                name= scan.next();
                 System.out.println("Enter new Salary");
-                sal=sc.nextDouble();
-                emp.setName(name);
-                emp.setSalary(sal);
+                sal= scan.nextDouble();
+                w.setName(name);
+                w.setSalary(sal);
                 System.out.println("Updated Details of employee are: ");
-                System.out.println(emp);
-                found=true;
+                System.out.println(w);
+                flag=true;
             }
         }
-        if(!found) {
+        if(!flag) {
             System.out.println("Employee is not present");
         }
         else {
             System.out.println("Employee details updated successfully !!");
         }
     }
-    //delete emp
+    //Calisan silme(delete emp)
     public void deleteEmp() {
         System.out.println("Enter id");
-        id=sc.nextInt();
-        boolean found=false;
-        Employee empdelete=null;
-        for(Employee emp:empset) {
-            if(emp.getId()==id) {
-                empdelete=emp;
-                found=true;
+        id= scan.nextInt();
+        boolean flag=false;
+        Employee deleted=null;
+        for(Employee w: employees) {
+            if(w.getId()==id) {
+                deleted=w;
+                flag=true;
             }
         }
-        if(!found) {
+        if(!flag) {
             System.out.println("Employee is not present");
         }
         else {
-            empset.remove(empdelete);
+            employees.remove(deleted);
             System.out.println("Employee deleted successfully!!");
         }
-    }
-    //add emp
-    public void addEmp() {
-        System.out.println("Enter id:");
-        id=sc.nextInt();
-        System.out.println("Enter name");
-        name=sc.next();
-        System.out.println("Enter age");
-        age=sc.nextInt();
-        System.out.println("enter Desiganation");
-        desiganation=sc.next();
-        System.out.println("Enter Department");
-        department=sc.next();
-        System.out.println("Enter sal");
-        sc.nextDouble();
-
-        Employee emp=new Employee(id, name, age, desiganation, department, sal);
-
-        empset.add(emp);
-        System.out.println(emp);
-        System.out.println("Employtee addeed successsfully");
-
     }
 
 }
